72cfac02fc2c6b7b9f2b646acfa45fc67628474f
pawlo903 <pawlo901@gmail.com>
2019-07-02 18:18:09 +0200

    Added sorting to the fetch-data page.
    Added requied packages of @angular such as material,cdk,animations. Also updated core package.
    Sorting works well with data from server.
    Modified app.module to include BrowserAnimationsModule, MatTableModule, MatSortModule .

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule, MatSortModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -22,6 +24,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject,OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-fetch-data',
@@ -8,7 +9,8 @@ import { HttpClient } from '@angular/common/http';
 export class FetchDataComponent implements OnInit {
   apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Szczecin,pl&appid=566c452675edab55f8961aa17ed935da&units=metric';
  
-  forecasts$: WeatherForecast;
+  forecasts$: WeatherForecast[];
+  sortedData: WeatherForecast[];
 
   constructor(private _http: HttpClient ) { }
 
@@ -18,7 +20,7 @@ export class FetchDataComponent implements OnInit {
   }
 
   getWeather() {
-    return this._http.get<WeatherForecast>(this.apiUrl);
+    return this._http.get<WeatherForecast[]>(this.apiUrl);
   }
   extractData(res: any) {
     let body = res;
@@ -28,6 +30,31 @@ export class FetchDataComponent implements OnInit {
   celsiusToFahrenheit(temp: number) {
     return Math.round(((temp * 9 / 5) + 32) * 100) / 100;
   }
+
+
+
+  sortData(sort: Sort) {
+    const data = this.forecasts$.slice();
+    if (!sort.active || sort.direction === '') {
+      this.forecasts$ = data;
+      return;
+    }
+
+    this.forecasts$ = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'Date': return compare(a.dt_txt, b.dt_txt, isAsc);
+        case 'TempC': return compare(a.main.temp, b.main.temp, isAsc);
+        case 'TempF': return compare(a.main.temp, b.main.temp, isAsc);
+        case 'Summary': return compare(a.weather[0].description, b.weather[0].description, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
 
 
